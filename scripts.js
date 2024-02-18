47132da1c7d67967090a1be0444bd4c43a7a9256
frederico nakajima <fredericonakajimanakajima@gmail.com>
2024-02-18 12:47:05 -0300

    ajuste

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-var senha = document.getElementById("inp")
+var senha = document.getElementById("input-01")
 function mostrarOcultarSenha(){
     if(senha.type=="password"){
         senha.type = "text"
